d0ddcc43c4ad2cf162fe867640b5c2cbe3dedad0
Hamit Mizrak <hamitmizrak@gmail.com>
2025-04-20 12:34:37 +0300

    data types

---
+++ src/main/java/com/hamitmizrak/lesson_1/_03_PrimitiveTypes.java
@@ -0,0 +1,26 @@
+package com.hamitmizrak.lesson_1;
+
+
+public class _03_PrimitiveTypes {
+    public static void main(String[] args) {
+        // Primitive Types
+        // Tamsayılar
+        // byte<short<int<long
+        // byte primitiveByte=
+    }
+}
+
+
+
+/*
+Primitive Type
+Wrapper   Type
+Operatorler
+Math
+String
+Conditional
+Loop
+Method
+Array
+OOP
+ */
