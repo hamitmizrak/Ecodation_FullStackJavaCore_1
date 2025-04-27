1e1ddd82d6d2aa5548c28195ba502ba4da587e5b
Hamit Mizrak <hamitmizrak@gmail.com>
2025-04-27 12:35:25 +0300

    şifre

--- src/main/java/com/hamitmizrak/week_2/_13_Exam_1.java
+++ src/main/java/com/hamitmizrak/week_2/_13_Exam_1.java
@@ -3,41 +3,72 @@ package com.hamitmizrak.week_2;
 import java.util.Scanner;
 
 public class _13_Exam_1 {
-    public static void main(String[] args) {
+
+    // Field (Global)
+    public String databaseUsername = "hamit", databasePassword = "root";
+    private Scanner sc = new Scanner(System.in);
+
+    // Kullanıcıdan username ve password almak
+    public String[] userValue() {
+        // Field (Local)
+        String dataPassword, dataUsername;
+        System.out.println("\nKullanıcı adınızı veya emailinizi giriniz...");
+        dataUsername = sc.nextLine();
+        System.out.println("\nŞifrenizi giriniz...");
+        dataPassword = sc.nextLine();
+
+        userValidation(dataPassword, dataUsername);
+
+        return new String[]{dataUsername, dataPassword};
+    }
+
+    // Kullanıcıdan alınan verilerin validation
+    public void userValidation(String username, String password) {
+        if(username.isBlank()) {
+            System.out.println("Kullanıcı adı boş geçilemez...");
+        }
+
+        if(password.isBlank()) {
+            System.out.println("Şifre boş geçilemez...");
+        }
+    }
+
+
+    public void mainProject() {
         // Password- Repassword (Şifre kontrolü)
         // Validation
         // Password Create
-        Scanner sc = new Scanner(System.in);
-
-        // Field
-        String databaseUsername="hamit",databasePassword="root";
-        String dataPassword,dataUsername;
+        _13_Exam_1 mainObject = new _13_Exam_1();
+        String[] methodUserPassword = mainObject.userValue();
+        String username = methodUserPassword[0];
+        String password = methodUserPassword[1];
 
         // Sonsuz Döngü
         for (; ; ) {
-            System.out.println("\nKullanıcı adınızı veya emailinizi giriniz...");
-            dataUsername = sc.nextLine();
-
-            System.out.println("\nŞifrenizi giriniz...");
-            dataPassword = sc.nextLine();
-
             // equals: birebir aynı()
             // equalsIgnoreCase: case sentive değildir.
-            if (databaseUsername.equalsIgnoreCase(dataUsername) &&  databasePassword.equals(dataPassword) ) {
+            if (databaseUsername.equalsIgnoreCase(username) && databasePassword.equals(password)) {
                 System.out.println("Şifreler aynı Admin sayfasına yönlendiriliyorsunuz");
                 return; // sonlandırma
             } else {
                 System.out.println("Kullanıcı adı veya Şifreler farklı");
+                mainProject();
             }
         } //end for
     }
-}
 
+    public static void main(String[] args) {
+        // Instance
+        _13_Exam_1 obj = new _13_Exam_1();
+        obj.mainProject();
+    }
+}
 
 
 /*
 Cumartesi
 ------------
+cast
 Exception
 Thread
 File IO/NIO
