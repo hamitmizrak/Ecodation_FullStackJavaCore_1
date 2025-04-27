4eaf2f0301dd41392932db2e7976df2e3b5f31af
Hamit Mizrak <hamitmizrak@gmail.com>
2025-04-27 10:26:20 +0300

    String examples

--- src/main/java/com/hamitmizrak/week_2/_14_Exam_1.java
+++ src/main/java/com/hamitmizrak/week_2/_14_Exam_1.java
@@ -1,8 +1,35 @@
 package com.hamitmizrak.week_2;
 
+import java.util.Scanner;
+
 public class _14_Exam_1 {
     public static void main(String[] args) {
-        // String
+        // Password- Repassword (Şifre kontrolü)
+        // Validation
+        // Password Create
+        Scanner sc = new Scanner(System.in);
+
+        // Field
+        String databaseUsername="hamit",databasePassword="root";
+        String dataPassword,dataUsername;
+
+        // Sonsuz Döngü
+        for (; ; ) {
+            System.out.println("\nKullanıcı adınızı veya emailinizi giriniz...");
+            dataUsername = sc.nextLine();
+
+            System.out.println("\nŞifrenizi giriniz...");
+            dataPassword = sc.nextLine();
+
+            // equals: birebir aynı()
+            // equalsIgnoreCase: case sentive değildir.
+            if (databaseUsername.equalsIgnoreCase(dataUsername) &&  databasePassword.equals(dataPassword) ) {
+                System.out.println("Şifreler aynı Admin sayfasına yönlendiriliyorsunuz");
+                return; // sonlandırma
+            } else {
+                System.out.println("Kullanıcı adı veya Şifreler farklı");
+            }
+        } //end for
     }
 }
 
