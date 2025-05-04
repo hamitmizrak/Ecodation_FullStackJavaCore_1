806c109f1c1560314fd51f9f9be85cf5a42d3fd4
Hamit Mizrak <hamitmizrak@gmail.com>
2025-05-04 10:36:56 +0300

    revize

---
+++ src/main/java/com/hamitmizrak/tutorials/week_2/_08_2_While_Loop.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.tutorials.week_2;
+
+public class _08_2_While_Loop {
+    public static void main(String[] args) {
+        // While Loop
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        // while sonsuz döngü
+        // while(true){}
+    }
+}
