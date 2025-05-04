8ae0cb0003579ecf6861dbf17070ba8b72dff3b1
Hamit Mizrak <hamitmizrak@gmail.com>
2025-05-04 10:37:42 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/exception/RegisterNotFoundException.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak.exception;
+
+// Bulunamazsa Fırlatılacak Özel Excepiton
+public class RegisterNotFoundException extends RuntimeException {
+
+    // Parametresiz Constructor
+    public RegisterNotFoundException() {
+        super("Kayıt bulunamadı");
+    }
+
+    // Parametreli Constructor
+    public RegisterNotFoundException(String message) {
+        super(message);
+    }
+}
